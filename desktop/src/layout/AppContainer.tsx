84c957641a30f00d498c98d1e31bddb5afcfdc44
CosmoArtsStore <cosmoartsstore@gmail.com>
2026-06-21 16:34:45 +0900

    サイドバーのテーマ切替を簡素化する

--- desktop/src/layout/AppContainer.tsx
+++ desktop/src/layout/AppContainer.tsx
@@ -285,7 +285,6 @@ export const AppContainer: React.FC = () => {
             )}
             <div className={`${styles.sidebarBlock} ${styles.sidebarBlockPush}`} />
             <div className={`${styles.sidebarBlock} ${styles.sidebarThemeSlider}`}>
-              <span className={styles.sidebarBlockLabel}>{NAV.SETTINGS}</span>
               <ThemeSelector themeId={themeId} setThemeId={setThemeId!} />
             </div>
           </div>
